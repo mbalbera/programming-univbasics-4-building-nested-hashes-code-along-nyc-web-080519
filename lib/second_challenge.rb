be47c7dbddab9ccc5f7108db317ea94323c3200d
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 15:14:10 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -25,27 +25,15 @@
 def second_challenge
 epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
+      :patriarch => {},
+      :matriarch => { },
+      :hero => {},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
+      :patriarch => { },
+      :matriarch => { },
+      :heroine => {},
       :heroine_friends => []
    }
 
