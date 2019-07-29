39844ac633dfa4bc11785570f64a8ed21b404577
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 15:20:03 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,13 +4,19 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{:age=>"17", :attitude=>"worried", :name=>"Benvolio"}, {:age=>"18", :attitude=>"hot-headed", :name=>"Mercutio"}]
+      :hero_friends => [
+      {:age=>"17", :attitude=>"worried", :name=>"Benvolio"}, 
+      {:age=>"18", :attitude=>"hot-headed", :name=>"Mercutio"}
+      ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{:age=>"30", :attitude=>"confused", :name=>"Steven"}, {:age=>"44", :attitude=>"worried", :name=>"Nurse"}]
+      :heroine_friends => [
+        {:age=>"30", :attitude=>"confused", :name=>"Steven"}, 
+        {:age=>"44", :attitude=>"worried", :name=>"Nurse"}
+        ]
    }
 }
 
