b373db465de6f693c5c509d35f464303a55f731d
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 14:45:15 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
    epic_tragedy = Hash.new()
-   epic_tragedy[:montague] = Hash.new()
+   epic_tragedy[:montague] = Hash.new(:patriarch,:matriarch, :hero, :hero_friends)
    epic_tragedy[:capulet] = Hash.new()
    return epic_tragedy
 end
