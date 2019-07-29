59579d3a619ebca519ad29b7d6026c138710913b
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 15:09:52 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,20 @@
 
   
-def second_challenge
-epic_tradgey = {
-  :montague => {
-    patriarch:'Lord Montague',
-    matriarch:'Lady Montague', 
-    hero:'Romeo', 
-    hero_friends:['Benvolio','Mercutio']
-  },
-  :capulet => {
-    patriarch:'Lord Capulet',
-    matriarch:'Lady Capulet', 
-    heroine:'Juliet', 
-    heroine_friends:['Steven','Nurse']
-  }
+# def second_challenge
+# epic_tradgey = {
+#   :montague => {
+#     patriarch:'Lord Montague',
+#     matriarch:'Lady Montague', 
+#     hero:'Romeo', 
+#     hero_friends:['Benvolio','Mercutio']
+#   },
+#   :capulet => {
+#     patriarch:'Lord Capulet',
+#     matriarch:'Lady Capulet', 
+#     heroine:'Juliet', 
+#     heroine_friends:['Steven','Nurse']
+#   }
 
-}
-return epic_tradgey
-end
+# }
+# return epic_tradgey
+# end
